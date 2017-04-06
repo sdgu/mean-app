b43f46f192a44f5d200ac37615a69ff6d01953f5
sdgu <sdgu000@gmail.com>
2017-04-05 23:38:26 -0400

    first for express test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component(
+{
+	selector: "my-app",
+	template:
+	`
+		<h1>{{title}}</h1>
+		<nav>
+			<a routerLink="/dashboard">Dashboard</a>
+			<a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+
+		</nav>
+		<router-outlet></router-outlet>
+	`,
+	styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent
+{
+	title = "Tour";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
 
-import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from "./hero-detail.component";
+import {HeroesComponent} from "./heroes.component";
+import {DashboardComponent} from "./dashboard.component";
+import {HeroService} from "./hero.service";
+
+import {AppRoutingModule} from "./app-routing.module";
+
+@NgModule(
+{
+  imports:      
+  [ 
+	  BrowserModule,
+	  FormsModule,
+    AppRoutingModule
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+  declarations: 
+  [ 
+  	AppComponent,
+    DashboardComponent,
+  	HeroDetailComponent,
+    HeroesComponent,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers:
+  [
+    HeroService,
+  ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
